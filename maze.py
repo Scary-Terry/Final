2b5e4877ded048d3d7bdef92824f060af45c40de
Sam Ruchti <samuelpruchti@gmail.com>
2018-05-31 13:50:34 -0400

    blocks layed out

--- maze.py
+++ maze.py
@@ -1,5 +1,25 @@
 import tkinter as tk
 
+walls_top = [[0, 0, 76.25, 15], [76.25, 0, 152.5, 15], [152.5, 0, 228.75, 15],
+             [305, 0, 381.25, 15], [381.25, 0, 457.5, 15], [457.5, 0, 533.75, 15],
+             [533.75, 0, 610, 15]]
+walls_bottom = [[0, 385, 76.25, 400], [76.25, 385, 152.5, 400], [152.5, 385, 228.75, 400],
+                [305, 385, 381.25, 400], [381.25, 385, 457.5, 400], [457.5, 385, 533.75, 400],
+                [533.75, 385, 610, 400]]
+walls_right = [[595, 15, 610, 89], [595, 89, 610, 163], [595, 163, 610, 237],
+               [595, 237, 610, 311], [595, 311, 610, 385]]
+walls_left = [[0, 15, 15, 89], [0, 89, 15, 163], [0, 163, 15, 237],
+              [0, 237, 15, 311], [0, 311, 15, 385]]
+walls_inner = [[76.25, 15, 91.25, 89], [76.25, 89, 152.5, 104], [152.5, 89, 228.75, 104],
+               [228.75, 89, 243.75, 163], [228.75, 163, 305, 178], [305, 163, 381.25, 178],
+               [305, 89, 320, 163], [381.25, 15, 396.25, 89], [381.25, 89, 457.5, 104],
+               [533.75, 89, 595, 104], [381.25, 163, 457.5, 178], [457.5, 163, 533.75, 178],
+               [457.5, 163, 472.5, 237], [457.5, 237, 533.75, 252], [381.25, 237, 457.5, 252],
+               [228.75, 237, 305, 252], [228.75, 237, 243.75, 311], [228.75, 311, 305, 326],
+               [305, 311, 381.25, 326], [381.25, 311, 457.5, 326], [457.5, 311, 533.75, 326],
+               [457.5, 311, 472.5, 385], [152.5, 237, 228.75, 252], [76.25, 237, 152.5, 252],
+               [152.5, 163, 167.5, 237], [76.25, 163, 152.5, 178], [76.25, 237, 152.5, 252],
+               [76.25, 311, 91.25, 385], [76.25, 311, 152.5, 326]]
 class GameObject(object):
     def __init__(self, canvas, item):
         self.canvas = canvas
@@ -17,126 +37,124 @@ class GameObject(object):
 
                 
 class avatar(GameObject):
-    def __init__(self,canvas, x, y):
-        self.radius = 5
-        self.ball = None
+    def __init__(self, canvas, x, y):
+        self.radius = 6
         item = canvas.create_oval(x-self.radius, y-self.radius,
                                   x+self.radius, y+self.radius,
-                                  fill = 'red')
+                                  fill = 'white')
         super(avatar, self).__init__(canvas, item)
-
+        
     def set_ball(self, ball):
         self.ball = ball
 
-    def move(self, offset):
+    def move(self, x_offset, y_offset):  
         coords = self.get_position()
-        width = self.canvas.winfo_width()
-        if coords[0] +offset >= 0 and coords[2] + offset <=width:
-            super(avatar, self).move(offset, 0)
-            
+        super(avatar, self).move(x_offset, y_offset)        
 
 
 class Brick(GameObject):
-    COLORS = {1: '#999999', 2: '#555555', 3:'#222222'}
 
-    def __init__(self, canvas, x, y, hits):
-        self.width = 20
-        self.height = 75
-        self.hits = hits
-        color = Brick.COLORS[hits]
-        item = canvas.create_rectangle(x - self.width / 2,
-                                       y - self.height / 2,
-                                       x + self.width / 2,
-                                       y + self.height / 2,
-                                       fill = color, tags= 'brick')
+    def __init__(self, canvas, x, y, xmax, ymax):
+        item = canvas.create_rectangle(x, y, xmax, ymax, fill = 'red', tags= 'brick')
         super(Brick, self).__init__(canvas, item)
-
-    def hit(self):
-
-        self.hits -=1
-        if self.hits == 0:
-            self.delete()
-        else:
-            self.canvas.itemconfig(self.item,
-                                   fill = Brick.COLORS[self.hits])
 
 class Game(tk.Frame):
     def __init__(self, master):
         super(Game, self).__init__(master)
-        self.lives = 3
-        self.width = 610
-        self.height = 400
-        self.canvas = tk.Canvas(self, bg='#aaaaff',
+        self.width = 1920
+        self.height = 1080
+        self.canvas = tk.Canvas(self, bg='blue',
                                 width = self.width,
                                 height=self.height,)
         self.canvas.pack()
         self.pack()
-
         self.items = {}
         self.ball = None
-        self.avatar = avatar(self.canvas, self.width/2, 326)
+        self.avatar = avatar(self.canvas, 266.875, 392.5)
         self.items[self.avatar.item] = self.avatar
-        for x in range(5, self.width - 5, 75):
-            self.add_brick(x + 37.5, 50, 2)
-            self.add_brick(x + 37.5, 70, 1)
-            self.add_brick(x + 37.5, 90, 1)
-
+        ########################
+        for values in walls_top:
+            x, y, xmax, ymax = values
+            self.add_brick(x, y, xmax, ymax)
+        for values in walls_bottom:
+            x, y, xmax, ymax = values
+            self.add_brick(x, y, xmax, ymax)
+        for values in walls_right:
+            x, y, xmax, ymax = values
+            self.add_brick(x, y, xmax, ymax)
+        for values in walls_left:
+            x, y, xmax, ymax = values
+            self.add_brick(x, y, xmax, ymax)
+        for values in walls_inner:
+            x, y, xmax, ymax = values
+            self.add_brick(x, y, xmax, ymax)
+        ########################
         self.hud = None
-        self.setup_game()
         self.canvas.focus_set()
 
-        self.canvas.bind('<Left>', lambda _: self.avatar.move(-10))
-        self.canvas.bind('<Right>', lambda _: self.avatar.move(10))
-        self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 10))
-        
-    def setup_game(self):
-        self.update_lives_text()
-        self.text = self.draw_text(300, 200,
-                                   'Press Space to start')
-        self.canvas.bind('<space>', lambda _: self.start_game())
+        self.canvas.bind('<Left>', lambda _: self.avatar.move(-7.5, 0))
+        self.canvas.bind('<Right>', lambda _: self.avatar.move(7.5, 0))
+        self.canvas.bind('<Up>', lambda _: self.avatar.move(0, -7.5))
+        self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 7.5))
 
-    def add_brick(self, x, y, hits):
-        brick = Brick(self.canvas, x, y, hits)
+        for values in walls_top:
+            x, y, xmax, ymax = values
+            ball_coords = self.avatar.get_position()
+            if x <= ball_coords <= xmax:
+                self.canvas.bind('<Left>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Right>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 0))
+            if y <= ball_coords <= ymax:
+                self.canvas.bind('<Left>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Right>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 0))
+        for values in walls_left:
+            x, y, xmax, ymax = values
+            ball_coords = self.avatar.get_position()
+            print(ball_coords)
+            if x <= ball_coords <= xmax:
+                self.canvas.bind('<Left>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Right>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 0))
+            if y <= ball_coords <= ymax:
+                self.canvas.bind('<Left>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Right>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 0))
+        for values in walls_right:
+            x, y, xmax, ymax = values
+            ball_coords = self.avatar.get_position()
+            if x <= ball_coords <= xmax:
+                self.canvas.bind('<Left>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Right>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 0))
+            if y <= ball_coords <= ymax:
+                self.canvas.bind('<Left>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Right>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 0))
+        for values in walls_bottom:
+            x, y, xmax, ymax = values
+            ball_coords = self.avatar.get_position()
+            if x <= ball_coords <= xmax:
+                self.canvas.bind('<Left>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Right>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 0))
+            if y <= ball_coords <= ymax:
+                self.canvas.bind('<Left>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Right>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Up>', lambda _: self.avatar.move(0, 0))
+                self.canvas.bind('<Down>', lambda _: self.avatar.move(0, 0))
+    def add_brick(self, x, y, xmax, ymax):
+        brick = Brick(self.canvas, x, y, xmax, ymax)
         self.items[brick.item] = brick
-
-    def draw_text(self, x, y, text, size='40'):
-        font = ('Helvetica', size)
-        return self.canvas.create_text(x, y, text=text, font=font)
-
-    def update_lives_text(self):
-        text = 'Lives: %s' %self.lives
-        if self.hud is None:
-            self.hud = self.draw_text(50, 20, text, 15)
-        else:
-            self.canvas.itemconfig(self.hud, text=text)
-
-    def start_game(self):
-        self.canvas.unbind('<space>')
-        self.canvas.delete(self.text)
-        self.game_loop()
-
-    def game_loop(self):
-        self.check_collisions()
-        num_bricks = len(self.canvas.find_withtag('brick'))
-        if num_bricks == 0:
-            self.ball.speed = None
-            self.draw_text(300, 200, 'You Win!')
-        elif self.ball.get_position()[3] >= self.height:
-            self.ball.speed = None
-            self.lives -= 1
-            if self.lives < 0:
-                self.draw_text(300, 200, 'Game Over')
-            else:
-                self.after(1000, self.setup_game)
-        else:
-            self.ball.update()
-            self.after(50, self.game_loop)
-
-    def check_collisions(self):
-        ball_coords = self.ball.get_position()
-        items = self.canvas.find_overlapping(*ball_coords)
-        objects = [self.items[x] for x in items if x in self.items]
-        self.ball.collide(objects)
+    
+        
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Maze!')
